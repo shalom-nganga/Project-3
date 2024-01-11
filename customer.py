42d43fa3302554fb3eb7f05146fda64143b50f8f
shalom-nganga <minnomacharia@gmail.com>
2024-01-11 10:26:35 +0300

    edited alembic.ini

--- customer.py
+++ customer.py
@@ -1,16 +1,16 @@
-# from sqlalchemy import Column, Integer, String
-# from sqlalchemy.orm import relationship
-# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-# Base = declarative_base()
+Base = declarative_base()
 
-# class Customer(Base):
-#     __tablename__ = 'customers'
-#     id = Column(Integer, primary_key=True)
-#     firstName = Column(String)
-#     lastName = Column(String)
-#     phoneNumber = Column(Integer)
-#     passWord = Column(String)
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer, primary_key=True)
+    firstName = Column(String)
+    lastName = Column(String)
+    phoneNumber = Column(Integer)
+    passWord = Column(String)
 
-#     bookings = relationship("Booking", back_populates="customer")
+    bookings = relationship("Booking", back_populates="customer")
 
